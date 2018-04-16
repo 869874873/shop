db84b1c85a79f0d431f3e138d06eb7f9522909de
869874873 <869874873@qq.com>
2018-04-16 09:34:25 +0800

    1

--- src/main/webapp/js/car.js
+++ src/main/webapp/js/car.js
@@ -1,4 +1,22 @@
 var arr = [];
+$(function() {
+	$.ajax({
+		url: 'ctrl/user/selectUserById',
+		success: function (data) {
+			$("#panal").accordion('add', {  
+		        title: '商品展示',  
+		        content: "<div style = \"float:left;margin-left : 20px;margin-top: 20px;\">" +
+		        		"<img id=\"img_1\" src = \"images/20170619160710.gif\" style = \"width:200px;height:200px;\">" +
+		        		"<div style = \"text-align: center;font-size : 15px;margin-top:10px;\">" +
+		        		"name : <span id = \"name_1\">商品1</span></div>" +
+		        		"<div style = \"text-align: center;font-size : 15px;margin-top:10px;color : red;\">" +
+		        		"price : <span id  = \"price_1\">￥50</span></div>" +
+		        		"<div style = \"text-align: center;font-size : 15px;margin-top:10px;\">" +
+		        		"<a onclick = \"addCar(1)\">加入购物车</a></div></div>"  
+		    });
+		}
+	})
+});
 // 前往结算页面
 function gotoPay() {
 	location.href = "pay.jsp";
